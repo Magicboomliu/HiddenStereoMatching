04c429b35a64fc9b80d87e2e929e3b3641d197bb
Zihua Liu <zliu@ok.sc.e.titech.ac.jp>
2024-01-12 17:58:50 +0900

    add

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
-# Diffusion Modedls  
-
-## Dependencies (Creat Env) 
+### Dependencies (Creat Env) 
 
 Install **Diffusers** and the **Transformers** and **Pytorch** 
+
+* Pytorch=1.X
 ```
-conda create -n diffusion python=3.9 
+conda create -n diffusion python=3.10
 conda activate diffusion
-pip install -r requirements.txt
+pip install -r requriements_torch1x.txt
 ```
-Install **xformer** properly to enable efficient transformers.(Options)   
-**Note** that install from the source may cost a lot of time depend on your device.
+* Pytorch=2.0.1
+
 ```
-conda install xformers -c xformers
-# from source
+conda create -n diffusion python=3.10
+conda activate diffusion
+pip install -r requriements_torch20.txt
+```
+
+Install **xformers**  
+* Pytorch=1.X
+```
 pip install -v -U git+https://github.com/facebookresearch/xformers.git@main#egg=xformers
 ```  
+* Pytorch=2.0.1
+```
+pip install xformers==0.0.20
+```  
 
-#### Examples 
-[Stable Diffsuion Fine-Tune: Pokemone Generation(Text2Image)](https://magicboomliu.github.io//posts/2012/08/blog-post-4/)
+
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,20 +1,53 @@
-run_diffusion(){
+LAUNCH_SD_FINETUNE_MINE(){
 cd ..
-CUDA_VISIBLE_DEVICES=0 python run_diffusion.py
+cd text_to_images
+
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export dataset_name="lambdalabs/pokemon-blip-captions"
+
+
+accelerate launch --mixed_precision="fp16"  --multi_gpu stable_fine_tune_accelate.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --dataset_name=$dataset_name \
+  --use_ema \
+  --resolution=512 --center_crop --random_flip \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --gradient_checkpointing \
+  --max_train_steps=15000 \
+  --learning_rate=1e-05 \
+  --max_grad_norm=1 \
+  --use_8bit_adam \
+  --lr_warmup_steps=0 \
+  --output_dir="sd-pokemon-model" 
 }
 
-inference_diffusion(){
+NoteBook_LAUNCH(){
 cd ..
-CUDA_VISIBLE_DEVICES=0 python inference.py
+cd text_to_images
+
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export dataset_name="lambdalabs/pokemon-blip-captions"
+python stable_fine_tune_notebook.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --dataset_name=$dataset_name \
+  --use_ema \
+  --resolution=512 --center_crop --random_flip \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --gradient_checkpointing \
+  --max_train_steps=15000 \
+  --learning_rate=1e-05 \
+  --max_grad_norm=1 \
+  --use_8bit_adam \
+  --lr_warmup_steps=0 \
+  --output_dir="sd-pokemon-model" 
+
+
+
 }
 
-inference_diffusion_conditioned(){
-cd ..
-CUDA_VISIBLE_DEVICES=0 python run_diffusion_condition.py
 
-}
+NoteBook_LAUNCH
 
-
-# inference_diffusion
-# run_diffusion
-inference_diffusion_conditioned
+# LAUNCH_SD_FINETUNE_MINE
